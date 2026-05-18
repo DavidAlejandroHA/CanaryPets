70cc85bf782fde2c88b07be92f3e0cc0ac98078c
David Alejandro <99730787+DavidAlejandroHA@users.noreply.github.com>
2026-05-18 21:55:28 +0100

    Eliminado método health() ambiguo

--- backend/src/main/java/com/canarypets/backend/controllers/MainController.java
+++ backend/src/main/java/com/canarypets/backend/controllers/MainController.java
@@ -35,9 +35,4 @@ public class MainController {
     public String home(@NotNull Model model) {
         return "redirect:/";
     }
-
-    @GetMapping("/") // Para el healthcheck (no es necesario del todo_ pero por si acaso)
-    public String health() {
-        return "OK";
-    }
 }
